0886b59ede4496a434a7eff10fc15dc3282d9581
edgyio <108406475+edgyio@users.noreply.github.com>
2023-06-16 15:25:41 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt install python
+wget https://packages.microsoft.com/config/debian/10/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
+pwsh
